71f06dc76029254e482d768dc8d48ea567ae8a18
Samuel Moraes <samuelm.moraes@hotmail.com>
2023-06-16 13:48:18 -0300

    modified:   Main.py 	new file:   global_funcion.py

--- Main.py
+++ Main.py
@@ -1,19 +1,31 @@
-custo = float(input("Digite o total da compra: "))
-val = float(input("Digite o valor recebido: "))
-val = val - custo
+import global_funcion
 
-if val > 0:
-  notas = [100, 50, 20, 10, 5]
-  notas.sort()
-  notas.reverse() 
+numNotas = []
+numMoedas = []
 
-  moedas = [1, 0.50, 0.25, 0.10, 0.05, 0.01]
-  moedas.sort()
-  moedas.reverse()
+# Valores que podem ser recebidos
 
-  numNotas = []
-  numMoedas = []
+# Notas
+notas = [100, 50, 20, 10, 5]
+notas.sort()
+notas.reverse() 
 
+# Moedas
+moedas = [1, 0.50, 0.25, 0.10, 0.05, 0.01]
+moedas.sort()
+moedas.reverse()
+
+print('''
+|############################################|
+|  Bem vindo ao sisteme de calculo de Troco  |
+|############################################|
+|Criado por Samuel Moraes                    |
+|############################################|
+''')
+
+global_funcion.calcula_troco()
+
+'''
   for i in notas:
     numNotas.append(val/i)
     val %= i
@@ -34,3 +46,4 @@ if val > 0:
   
 else:
   print("Valor do pagamento é inferiror ao valor da compra")
+'''
