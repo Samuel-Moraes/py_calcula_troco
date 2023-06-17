806c4b5283e27f92577c0f59529110593c955df9
samuel <samuelm.moraes@hotmail.com>
2023-06-16 22:41:42 -0300

    modified:   Main.py 	modified:   global_funcion.py

--- Main.py
+++ Main.py
@@ -1,19 +1,8 @@
-import global_funcion
+import global_funcion 
 
-numNotas = []
-numMoedas = []
-
-# Valores que podem ser recebidos
-
-# Notas
-notas = [100, 50, 20, 10, 5]
-notas.sort()
-notas.reverse() 
-
-# Moedas
 moedas = [1, 0.50, 0.25, 0.10, 0.05, 0.01]
-moedas.sort()
-moedas.reverse()
+notas = [100, 50, 20, 10, 5]
+
 
 print('''
 |############################################|
@@ -24,25 +13,5 @@ print('''
 ''')
 
 val = global_funcion.calcula_troco()
-
-
-for i in notas:
-    numNotas.append(val/i)
-    val %= i
-
-for i in moedas:
-      numMoedas.append(val/i)
-      val %= i
-
-print(" ") 
-
-for i in range(len(notas)):
-    print("Notas de %d = %d" % (notas[i], numNotas[i]))
-
-print("")
-
-for i in range(len(moedas)):
-    print("Moedas de %a = %d" % (moedas[i], numMoedas[i]))
-  
-else:
-  print("Valor do pagamento é inferiror ao valor da compra")
+numNotas, numMoedas = global_funcion.calcula_notas(val)
+global_funcion.mostrador(notas, moedas, numNotas, numMoedas)
--- global_funcion.py
+++ global_funcion.py
@@ -1,4 +1,4 @@
-def calcula_troco():
+def calcula_troco(): 
     repetidor = 1
     total_compra = float(input("Digite o total da compra: "))
     while repetidor:
@@ -11,4 +11,36 @@ def calcula_troco():
             repetidor = 0
             return troco
         
- 
+def calcula_notas(val):
+    numNotas = []
+    numMoedas = []
+
+    moedas = [1, 0.50, 0.25, 0.10, 0.05, 0.01]
+    notas = [100, 50, 20, 10, 5]
+    moedas.sort()
+    moedas.reverse()
+
+    notas.sort()
+    notas.reverse() 
+
+    for i in notas:
+        numNotas.append(val/i)
+        val %= i
+
+
+    for i in moedas:
+        numMoedas.append(val/i)
+        val %= i
+    
+    return numNotas, numMoedas
+
+def mostrador(notas, moedas, numNotas, numMoedas):
+    print('')
+    for i in range(len(notas)):
+        print("Notas de %d = %d" % (notas[i], numNotas[i]))
+
+    print("")
+
+    for i in range(len(moedas)):
+        print("Moedas de %a = %d" % (moedas[i], numMoedas[i]))
+    print('')
